028a3079993e25078055fddca6d2131393b9a0f0
Justin Williams <justinw@me.com>
2022-09-13 16:22:01 -0600

    Update install script for macOS

--- install.sh
+++ install.sh
@@ -1,20 +1,48 @@
 #!/usr/bin/env sh
 #
 # Configure a new machine with the dotfiles and configuration from this repository.
+# Install script shamelessly stolen and updated from viggo-gascou/dotfiles.
 
 set -e
 
-CHEZMOI="/usr/local/bin/chezmoi"
-CHEZMOI_DIR="$(dirname "$CHEZMOI")"
+RED='\033[1;31m' # bold red
+GREEN='\033[32;1m' # bold green
+BOLD='\033[1m' # bold
+NC='\033[0m' # no Color
 
-if [ -x "$(command -v brew)" ]; then
-    CHEZMOI="$(command -v chezmoi)"
-    CHEZMOI_DIR="$(dirname "$CHEZMOI")"
+# My GitHub account.
+readonly GITHUB_USERNAME="justin"
+
+# The operating system this script will run on.
+readonly PLATFORM="$( uname )"
+
+if [ $PLATFORM = "Darwin" ]; then
+  if [ "${xcode-select -p 1>/dev/null;echo $?}" = 2 ]; then # only install Xcode Command Line Tools if not installed
+    echo "${BOLD}⏳ Installing Xcode Command Line Tools...${NC}"
+    xcode-select --install
+  else
+    echo "${GREEN}✅ Xcode Command Line Tools already installed.${NC}"
+  fi
+
+  if command -v brew >/dev/null 2>&1; then # only install Homebrew if not installed
+    echo "${GREEN}✅ Homebrew already installed.${NC}"
+
+    if command -v chezmoi >/dev/null 2>&1; then # only install chezmoi if not installed
+      echo "${GREEN}✅ Chezmoi already installed.${NC}"
+    else
+      echo "${BOLD}Installing chezmoi via homebrew.${NC}"
+      brew install chezmoi
+    fi
+  else
+    echo "${BOLD}⏳ Installing Homebrew...${NC}"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "${BOLD}Installing chezmoi via homebrew.${NC}"
+    brew install chezmoi
+  fi
+
+  echo "${BOLD}🛠 Setting up dotfiles using chezmoi...${NC}"
+  exec chezmoi init --apply $GITHUB_USERNAME
 else
-    echo "Downloading chezmoi project for dotfiles management."
-    mkdir -p "$CHEZMOI_DIR"
-    sh -c "$(curl -fsLS git.io/chezmoi)" -- -b "$CHEZMOI_DIR"
+  echo "${BOLD}🛠 Setting up dotfiles using chezmoi...${NC}"
+  sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply $GITHUB_USERNAME
 fi
-
-echo "Configuring dotfiles."
-"$CHEZMOI" init --apply justin
