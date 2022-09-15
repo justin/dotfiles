c17a8e1c510e6342d6d6b81b52a6b382308abf15
Justin Williams <justinw@me.com>
2022-09-15 16:16:27 -0600

    Move xcode templates into repo

---
+++ files/xcode/templates/file/Source/JWW - SwiftUI View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER___()
+    }
+}
+
+@available(iOS 14.0, macCatalyst 14.0, macOS 11.0, *)
+struct ___FILEBASENAMEASIDENTIFIER____LibraryViewContents: LibraryContentProvider {
+    var views: [LibraryItem] {
+        LibraryItem(___FILEBASENAMEASIDENTIFIER___())
+    }
+}
---
+++ files/xcode/templates/file/Source/JWW - Unit Test Case.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import <#MODULENAME#>
+
+final class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testZero() throws {
+        XCTFail("Tests not yet implemented in ___FILEBASENAMEASIDENTIFIER___")
+    }
+}
