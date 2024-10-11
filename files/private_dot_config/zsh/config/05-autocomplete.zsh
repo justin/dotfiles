0934343bca36247605d820f2314d58f89930c3e0
Justin Williams <justinw@me.com>
2024-10-11 10:17:11 -0600

    Load zsh-autocomplete after zsh-autocomplete
    Resolves this issue on my Ubuntu box.
    
    https://github.com/zsh-users/zsh-syntax-highlighting/issues/951

--- files/private_dot_config/zsh/config/05-autocomplete.zsh
+++ files/private_dot_config/zsh/config/05-autocomplete.zsh
@@ -1,12 +1,3 @@
-# Source zsh-autocomplete
-# https://github.com/marlonrichert/zsh-autocomplete
-if [[ -e "$HOME/.cache/zsh/external/zsh-autocomplete/zsh-autocomplete.plugin.zsh" ]]; then
-  if is_linux; then
-    export skip_global_compinit=1
-  fi
-  source "$HOME/.cache/zsh/external/zsh-autocomplete/zsh-autocomplete.plugin.zsh" || true
-fi
-
 # Source zsh-syntax-highlighting extension.
 # https://github.com/zsh-users/zsh-syntax-highlighting
 if [[ -e "$HOME/.cache/zsh/external/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
@@ -17,4 +8,13 @@ fi
 # https://github.com/zsh-users/zsh-autosuggestions
 if [[ -e "$HOME/.cache/zsh/external/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
   source "$HOME/.cache/zsh/external/zsh-autosuggestions/zsh-autosuggestions.zsh" || true
+fi
+
+# Source zsh-autocomplete
+# https://github.com/marlonrichert/zsh-autocomplete
+if [[ -e "$HOME/.cache/zsh/external/zsh-autocomplete/zsh-autocomplete.plugin.zsh" ]]; then
+  if is_linux; then
+    export skip_global_compinit=1
+  fi
+  source "$HOME/.cache/zsh/external/zsh-autocomplete/zsh-autocomplete.plugin.zsh" || true
 fi
