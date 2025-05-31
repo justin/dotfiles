db31b00742db4f8ea452ec4f928ff7e1b5e4fea7
Justin Williams <justinw@me.com>
2025-05-31 10:51:26 -0600

    Add password management install script

---
+++ .install-password-manager.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Install 1Password CLI upon launch.
+
+set -e
+
+source "$HOME/.local/share/chezmoi/files/functions/_logging"
+source "$HOME/.local/share/chezmoi/files/functions/_utilities"
+
+if [ -n "${CODESPACES}" ] || [ -n "${CI}" ]; then
+  info "Exit. Skip install-password-manager for non interactive shells."
+  exit
+fi
+
+if [ "$(hostname)" = "nas" ]; then
+  info "Exit. Skip install-password-manager for NAS."
+  exit
+fi
+
+if type_exists 'op'; then
+  exit 0
+fi
+
+is_macos || is_linux || fail "Unsupported OS. Only macOS and Linux are supported."
+info "Installing 1Password CLI on Linux"
+brew install 1password-cli
--- install.sh
+++ install.sh
@@ -54,8 +54,6 @@ else
   # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
   script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-  set -- init --apply --source="${script_dir}"
-
   echo "Running 'chezmoi $*'" >&2
-  exec "$chezmoi" "$@"
+  exec "$chezmoi" init --apply "--source=$script_dir"
 fi
