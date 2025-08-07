8b99eec524f6e459adf47d0804c1e273513b68a2
Justin Williams <justinw@me.com>
2025-08-07 08:47:07 -0600

    Use eza and bat

---
+++ files/private_dot_config/zsh/config/08-eza.zsh
@@ -0,0 +1,15 @@
+if type eza > /dev/null 2>&1; then
+  export EZA_CONFIG_DIR="$XDG_CONFIG_HOME/eza"
+  # If eza is available, use it instead of ls.
+  alias ls='eza'
+  alias l='ls -l'
+  alias la='ls -la'
+  alias ll='ls -l'
+
+else
+  # Otherwise, use the default ls command.
+  alias ls='ls --color=auto'
+  alias l='ls -l'
+  alias la='ls -la'
+  alias ll='ls -l'
+fi
--- files/private_dot_config/zsh/config/color.zsh
+++ files/private_dot_config/zsh/config/color.zsh
@@ -15,6 +15,17 @@ export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK
 export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
 export PR_BOLD_WHITE PR_BOLD_BLACK
 
+
+# LSCOLORS: Customize 'ls' colors in BSD/macOS. Each pair sets foreground/background for file types.
+# Format: aabbccddeeff, where:
+#   aa = directory, bb = symbolic link, cc = socket, dd = pipe, ee = executable, ff = block special,
+#   gg = character special, hh = executable with setuid, ii = executable with setgid, jj = directory writable to others, k = directory sticky & writable to others
+#
+# Color codes: a = black, b = red, c = green, d = brown, e = blue, f = magenta, g = cyan, h = light grey,
+#   A = bold black, B = bold red, C = bold green, D = bold brown, E = bold blue, F = bold magenta, G = bold cyan, H = bold light grey, x = default
+# Example below:
 unset LSCOLORS
 export CLICOLOR=1
 export LSCOLORS="cxfxcxdxbxegedabagacad"
+#  dir  sym  sock pipe exec block char suid sgid  wdir  stky
+#  cx   fx   cx   dx   bx   eg   ed   ab   ag    ac   ad
