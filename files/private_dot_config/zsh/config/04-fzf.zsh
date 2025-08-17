1dd88c4a16e82bb560ae53f55cf4c5614168e92b
Justin Williams <justinw@me.com>
2025-08-17 11:21:43 -0600

    fzf config updates
    [ci skip]

--- files/private_dot_config/zsh/config/04-fzf.zsh
+++ files/private_dot_config/zsh/config/04-fzf.zsh
@@ -16,9 +16,34 @@ readonly WHITE="#FFFFFF"
 
 export FZF_DEFAULT_OPTS="--color=fg:${FG},bg:${BG},hl:${MAGENTA},fg+:${FG},bg+:${BG_HIGHLIGHT},hl+:${GREEN},info:${YELLOW},prompt:${CYAN},pointer:${CYAN},marker:${CYAN},spinner:${CYAN},header:${CYAN}"
 
+export FZF_CTRL_R_OPTS="--preview=\"bat --color=always {}\" \
+--preview-window down:3:hidden:wrap \
+--bind '?:toggle-preview' \
+"
+
+# Always use a tmux popup for fzf
+export FZF_TMUX_OPTS="-p"
+
 # -- Use fd instead of find --
-export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git --preview=\"bat --color=always {}\""
-export FZF_CTRL_T_COMMAND=\"$FZF_DEFAULT_COMMAND\"
-export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git --preview=\"bat --color=always {}\""
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+
+# -- Use fd for fzf's cd command --
+# This command will list directories, including hidden ones, and strip the current working directory prefix.
+# It will also exclude the .git directory.
+export FZF_ALT_C_COMMAND="fd \
+--type=d \
+--hidden \
+--strip-cwd-prefix \
+--exclude .git"
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+
+fzf-history-widget-accept() {
+  fzf-history-widget
+  zle accept-line
+}
+# Bind executing the selected command to Ctrl+X Ctrl+R
+zle     -N     fzf-history-widget-accept
+bindkey '^X^R' fzf-history-widget-accept
 
 fi
