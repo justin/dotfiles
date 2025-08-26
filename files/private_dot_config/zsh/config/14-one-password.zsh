3b4c450b98c3e95ff12cb006b4180f7d6d8aed4a
Justin Williams <justinw@me.com>
2025-08-26 15:20:08 -0600

    Only source op plugins if they exist

--- files/private_dot_config/zsh/config/14-one-password.zsh
+++ files/private_dot_config/zsh/config/14-one-password.zsh
@@ -4,5 +4,7 @@ if is_cmd op; then
   eval "$(op completion zsh)"
   compdef _op op
 
+  # Load plugins if the file exists
+  [ -f "$XDG_CONFIG_HOME/op/plugins.sh" ] && \
   source "$XDG_CONFIG_HOME/op/plugins.sh"
 fi
