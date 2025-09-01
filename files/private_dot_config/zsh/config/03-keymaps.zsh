20b20dc0d374f77ffb88fdbe9f2d218c191e8a5a
Justin Williams <justinw@me.com>
2025-09-01 10:24:04 -0600

    asdf

--- files/private_dot_config/zsh/config/03-keymaps.zsh
+++ files/private_dot_config/zsh/config/03-keymaps.zsh
@@ -1,20 +1,20 @@
-bindkey '^[[1;5C' forward-word     # Ctrl + → (Right Arrow): Move forward one word
-bindkey '^[[1;5D' backward-word    # Ctrl + ← (Left Arrow): Move backward one word
-bindkey '^[[1~' beginning-of-line  # Home: Move to beginning of line
-bindkey '^[[4~' end-of-line        # End: Move to end of line
-bindkey '^[[H' beginning-of-line   # Home (alternate): Move to beginning of line
-bindkey '^[[F' end-of-line         # End (alternate): Move to end of line
-bindkey '^[[3~' delete-char        # Delete: Delete character under cursor
+# bindkey '^[[1;5C' forward-word     # Ctrl + → (Right Arrow): Move forward one word
+# bindkey '^[[1;5D' backward-word    # Ctrl + ← (Left Arrow): Move backward one word
+# bindkey '^[[1~' beginning-of-line  # Home: Move to beginning of line
+# bindkey '^[[4~' end-of-line        # End: Move to end of line
+# bindkey '^[[H' beginning-of-line   # Home (alternate): Move to beginning of line
+# bindkey '^[[F' end-of-line         # End (alternate): Move to end of line
+# bindkey '^[[3~' delete-char        # Delete: Delete character under cursor
 
-# zsh-autosuggestions keybindings
-# https://github.com/zsh-users/zsh-autosuggestions?tab=readme-ov-file#configuration
+# # zsh-autosuggestions keybindings
+# # https://github.com/zsh-users/zsh-autosuggestions?tab=readme-ov-file#configuration
 
-# Use Ctrl + Y to accept the autosuggestion
-bindkey '^Y' autosuggest-accept
-# Use Ctrl + J to accept the autosuggestion
-bindkey '^J'  autosuggest-execute
-# Use Ctrl + H to clear the autosuggestion
-bindkey '^H' autosuggest-clear
-# Use Ctrl + K to fetch the next suggestion
-bindkey '^K' autosuggest-fetch
+# # Use Ctrl + Y to accept the autosuggestion
+# bindkey '^Y' autosuggest-accept
+# # Use Ctrl + J to accept the autosuggestion
+# bindkey '^J'  autosuggest-execute
+# # Use Ctrl + H to clear the autosuggestion
+# bindkey '^H' autosuggest-clear
+# # Use Ctrl + K to fetch the next suggestion
+# bindkey '^K' autosuggest-fetch
 
